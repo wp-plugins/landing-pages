84c8c56257b82ff2158e1a04815c3a33582714bd
DavidWells <DavidWells@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-16 04:30:57 +0000

    Updating Styles to WordPress 3.8
    git-svn-id: https://plugins.svn.wordpress.org/landing-pages/trunk@822886 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- modules/module.language-support.php
+++ modules/module.language-support.php
@@ -1,11 +1,7 @@
 <?php
 
-if (!defined('LANDINGPAGES_TEXT_DOMAIN'))
-	define( 'LANDINGPAGES_TEXT_DOMAIN', 'inbound_now' ); 
-
 add_action('init' , 'lp_load_text_domain');
-
 function lp_load_text_domain()
 {
-	load_plugin_textdomain( LANDINGPAGES_TEXT_DOMAIN , false , LANDINGPAGES_PATH . 'shared/languages/landing-pages/' );
+	load_plugin_textdomain( LANDINGPAGES_TEXT_DOMAIN , false , LANDINGPAGES_PLUGIN_SLUG.'/shared/languages/landing-pages/' );
 }
